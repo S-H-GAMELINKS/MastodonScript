f490a986392a23d13941a97ccdba4b164e5a14b7
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-07 17:06:53 +0900

    add update shell

---
+++ update.sh
@@ -0,0 +1,3 @@
+su -l mastodon -c "cd live && git fetch && git stash && git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1) && git stash pop && gem install bundler && bundle install --deployment --without development test  && yarn install && RAILS_ENV=production bundle exec rails db:migrate && RAILS_ENV=production bundle exec rake mastodon:maintenance:add_static_avatars && RAILS_ENV=production bundle exec rails assets:precompile"
+
+sudo systemctl restart mastodon*

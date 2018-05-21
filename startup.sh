a40eecbb0e7423c89a71f0345676b719f62d022b
S.H <gamelinks007@gmail.com>
2018-05-21 18:46:16 +0900

    add startup.sh

---
+++ startup.sh
@@ -0,0 +1,27 @@
+# Add mastodon user　
+sudo adduser mastodon
+
+# apt update & upgrate
+sudo apt update && sudo apt upgrade
+
+# Add node.js Repository
+sudo apt -y install curl && sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+
+# Add Yarn Repository
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+
+# All need prigram install
+sudo apt -y install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev nginx redis-server redis-tools postgresql postgresql-contrib letsencrypt yarn libidn11-dev libicu-dev
+
+# Install rbenv
+sudo su -l mastodon "git clone https://github.com/rbenv/rbenv.git ~/.rbenv && cd ~/.rbenv && src/configure && make -C src && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc"
+
+# Restart bash
+exec bash
+
+# Install ruby-build as rbenv plugin
+sudo su -l mastodon "git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build && rbenv install 2.5.0 && rbenv global 2.5.0"
+
+sudo su -l mastodon "cd ~ && git clone https://github.com/tootsuite/mastodon.git live && cd ~/live && git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1) && gem install bundler && bundle install -j$(getconf _NPROCESSORS_ONLN) --deployment --without development test && yarn install --pure-lockfile"

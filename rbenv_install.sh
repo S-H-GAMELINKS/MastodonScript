557da52afacbaad924f31dfba740da39b302f18a
S.H <gamelinks007@gmail.com>
2018-07-26 16:00:52 +0900

    Create rbenv_install.sh

---
+++ rbenv_install.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv && cd ~/.rbenv && src/configure && make -C src && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc

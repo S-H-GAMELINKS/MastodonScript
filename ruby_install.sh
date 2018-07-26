cebd66bb47cd01ad33a3f84b7758c243ba5163e5
S.H <gamelinks007@gmail.com>
2018-07-26 17:08:08 +0900

    Update ruby_install.sh

--- ruby_install.sh
+++ ruby_install.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build && rbenv install 2.5.0 && rbenv global 2.5.0
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build && rbenv install 2.5.1 && rbenv global 2.5.1

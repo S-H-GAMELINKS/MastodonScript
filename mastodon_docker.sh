974ed94b5ccdc84a2c0065a56a7401ae83fc3f61
S.H <gamelinks007@gmail.com>
2018-07-28 18:42:33 +0900

    Update mastodon_docker.sh

--- mastodon_docker.sh
+++ mastodon_docker.sh
@@ -7,6 +7,7 @@ sudo apt install git
 sudo apt install docker.io
 sudo apt install docker-compose
 sudo apt install -y libssl-dev libreadline-dev 
+sudo apt install letsencrypt
 
 sudo adduser --ingroup docker docker
 
@@ -18,7 +19,13 @@ sudo su -l docker -c "git clone https://github.com/rbenv/ruby-build.git ~/.rbenv
 
 su - docker
 
-sudo su -l docker -c "cd ~ && git clone https://github.com/tootsuite/mastodon.git live && cd ~/live && git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1) && vi docker-compose.yml && cp .env.production.sample .env.production && docker-compose pull && docker-compose build && docker-compose run --rm web bundle exec rake mastodon:setup && docker-compose up -d"
+sudo su -l docker -c "cd ~ && git clone https://github.com/tootsuite/mastodon.git live && cd ~/live && git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1) && vi docker-compose.yml && cp .env.production.sample .env.production && docker-compose pull && docker-compose build"
+
+sudo su -l docker -c "docker-compose run --rm web rake secret && docker-compose run --rm web rake secret && docker-compose run --rm web rake secret"
+
+su - docker
+
+sudo su -l docker -c "cd ~/live && docker-compose run --rm web bundle exec rake mastodon:setup && docker-compose up -d"
 
 sudo vi /etc/nginx/sites-available/$1.conf
 cd /etc/nginx/sites-enabled
